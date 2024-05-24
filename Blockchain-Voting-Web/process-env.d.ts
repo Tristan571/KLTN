300edbc665b520181491824564c24fb0c4d2c6b0
Tristan571 <tringuyen572001@gmail.com>
2024-05-24 17:58:35 +0700

    Added my project

---
+++ Blockchain-Voting-Web/process-env.d.ts
@@ -0,0 +1,13 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// declare global {
+//     namespace NodeJS {
+//       interface ProcessEnv {
+//         [key: string]: string | undefined;
+//         DB_CONN_STRING: string,
+//         DB_NAME: string,
+//         VOTING_COLLECTION_NAME: string,
+//       }
+//     }
+//   }
